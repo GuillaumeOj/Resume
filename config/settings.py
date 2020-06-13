7c831328ca23af942d517d4b7d5b6584a3712bc6
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-06-13 17:35:34 +0200

    Start the application Resume

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["192.168.1.47"]
 # Application definition
 
 INSTALLED_APPS = [
+    "resume.apps.ResumeConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
