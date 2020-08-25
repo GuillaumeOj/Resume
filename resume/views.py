d167fb86cce082c80475be7b0b962129991c9e47
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-08-25 16:42:53 +0200

    Change the section about

--- resume/views.py
+++ resume/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
+from resume.models import About
 
-# Create your views here.
+
 def index(request):
-    return render(request, "resume/index.html")
+    resume = About.objects.all().first()
+    context = {"resume": resume}
+
+    return render(request, "resume/index.html", context=context)
