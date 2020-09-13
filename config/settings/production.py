e94a710656120370e4dc8ac80a6f1b9b37ef3547
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-09-13 11:47:03 +0200

    Update project settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,25 +1,39 @@
-"""
-Django settings for config project in local repository.
-"""
+"""Django settings for the remote project."""
 import os
 
 import dj_database_url
+from dotenv import find_dotenv
+from dotenv import load_dotenv
 
+# Import the base settings
 from .base import *
 
-# Local SECRET_KEY
-SECRET_KEY = os.environ["SECRET_KEY"]
 
-# Allow debug
-DEBUG = False
-
-# Computer adress in the local network
-ALLOWED_HOSTS = ["guillaume-ojardias.herokuapp.com", ".ojardias.io"]
-
-SECURE_SSL_REDIRECT = True
-
+ALLOWED_HOSTS = ["guillaume.ojardias.io", "www.ojardias.io"]
 
 # Heroku database
-DATABASE_URL = os.environ["DATABASE_URL"]
-DATABASES = {}
-DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+if os.getenv("ENV_HOST") == "HEROKU":
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    DEBUG = True if os.getenv("DEBUG") == "True" else False
+    MIDDLEWARE.append("whitenoise.middleware.WhiteNoiseMiddleware",)
+
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    DATABASES = {}
+    DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+else:
+    load_dotenv(find_dotenv())
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    DEBUG = True if os.getenv("DEBUG") == "True" else False
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": os.getenv("DATABASE_NAME"),
+            "USER": os.getenv("DATABASE_USER"),
+            "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+            "HOST": os.getenv("DATABASE_HOST"),
+            "PORT": os.getenv("DATABASE_PORT"),
+        }
+    }
--- manage.py
+++ manage.py
@@ -5,8 +5,10 @@ import sys
 
 
 def main():
-    # os.environ.setdefault("ENVIRONNEMENT", "development")
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+    if os.environ.get("ENV") == "PRODUCTION":
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
