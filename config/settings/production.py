14928de2dcb0b28235f5a7eaa846f30ff8093aad
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-09-14 10:03:49 +0200

    Change the production settings for deploying only on Digital Ocean

--- config/settings/production.py
+++ config/settings/production.py
@@ -11,29 +11,16 @@ from .base import *
 
 ALLOWED_HOSTS = ["guillaume.ojardias.io", "www.ojardias.io"]
 
-# Heroku database
-if os.getenv("ENV_HOST") == "HEROKU":
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    DEBUG = True if os.getenv("DEBUG") == "True" else False
-    MIDDLEWARE.append("whitenoise.middleware.WhiteNoiseMiddleware",)
-
-    DATABASE_URL = os.getenv("DATABASE_URL")
-    DATABASES = {}
-    DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
-else:
-    load_dotenv(find_dotenv())
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    DEBUG = True if os.getenv("DEBUG") == "True" else False
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": os.getenv("DATABASE_NAME"),
-            "USER": os.getenv("DATABASE_USER"),
-            "PASSWORD": os.getenv("DATABASE_PASSWORD"),
-            "HOST": os.getenv("DATABASE_HOST"),
-            "PORT": os.getenv("DATABASE_PORT"),
-        }
+load_dotenv(find_dotenv())
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = True if os.getenv("DEBUG") == "True" else False
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": os.getenv("DATABASE_HOST"),
+        "PORT": os.getenv("DATABASE_PORT"),
     }
+}
