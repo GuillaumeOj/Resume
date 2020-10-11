886403822e116c10e8d63bd9e47e302ae8e65d20
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-10-11 20:54:46 +0200

    feat(experience): use the model experience in templates for education

--- resume/views.py
+++ resume/views.py
@@ -8,9 +8,22 @@ from resume.models import Social
 
 def index(request):
     resume = About.objects.all().first()
+
     socials = Social.objects.filter(about=resume)
+
     pro_pathway = PathWay.objects.filter(title="Expérience").first()
     pro_experiences = Experience.objects.filter(about=resume, path=pro_pathway)
-    context = {"resume": resume, "socials": socials, "pro_experiences": pro_experiences}
+
+    education_pathway = PathWay.objects.filter(title="Formation").first()
+    education_experiences = Experience.objects.filter(
+        about=resume, path=education_pathway
+    )
+
+    context = {
+        "resume": resume,
+        "socials": socials,
+        "pro_experiences": pro_experiences,
+        "education_experiences": education_experiences,
+    }
 
     return render(request, "resume/index.html", context=context)
