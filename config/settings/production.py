5fb953c0345bcc8e7974bc9d9751ffda5330a236
Guillaume Ojardias <7831777+GuillaumeOj@users.noreply.github.com>
2021-01-16 12:18:50 +0100

    fix settings (#38)
    * fix(config): use same database for each config
    
    * chore: ignore missing docstrings
    
    * chore: rename travis settings to tests
    
    * chore: remove useless comment
    
    * chore: add default secret key for tests

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,25 +1,4 @@
-"""Django settings for the remote project."""
-import os
-
-from dotenv import find_dotenv
-from dotenv import load_dotenv
-
-# Import the base settings
 from .base import *  # noqa: F401, F403
 
 
 ALLOWED_HOSTS = ["ojardias.io", "guillaume.ojardias.io", "www.ojardias.io"]
-
-load_dotenv(find_dotenv())
-SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = True if os.getenv("DEBUG") == "True" else False
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("DATABASE_NAME"),
-        "USER": os.getenv("DATABASE_USER"),
-        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
-        "HOST": os.getenv("DATABASE_HOST"),
-        "PORT": os.getenv("DATABASE_PORT"),
-    }
-}
